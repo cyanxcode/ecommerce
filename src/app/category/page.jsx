06a38537d12e15f91827f4df3858b233f446d717
cyanxcode <151661246+cyanxcode@users.noreply.github.com>
2024-09-21 15:25:38 +0530

    Potential fix

--- src/app/category/page.jsx
+++ src/app/category/page.jsx
@@ -1,34 +1,37 @@
 "use client";
 import Cards from "@/components/Category/Cards";
 import Link from "next/link";
-import { db, app } from "@/lib/firebase";
+import { db } from "@/lib/firebase";
 
 import { useSearchParams } from "next/navigation";
 import { IoArrowBack, IoBagOutline } from "react-icons/io5";
 import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
-
+if (typeof window !== "undefined") {
+  // Code that references window here
+}
 export default function Category() {
   const searchParams = useSearchParams();
 
   const title = searchParams.get("group");
   const [collections, setCollections] = useState<any>([]);
-  const [docData, setDocData] = useState(null);
 
   useEffect(() => {
     getCollections();
   }, []);
   const getCollections = async () => {
-    const querySnapshot = await getDocs(collection(db, "categories"));
+    if (typeof window !== "undefined") {
+      const querySnapshot = await getDocs(collection(db, "categories"));
 
-    const productData = querySnapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-    const filteredData = productData.filter(
-      (x: any) => x.group == title?.toLowerCase()
-    );
-    setCollections(filteredData);
+      const productData = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      const filteredData = productData.filter(
+        (x) => x.group == title?.toLowerCase()
+      );
+      setCollections(filteredData);
+    }
   };
   return (
     <>
@@ -42,7 +45,7 @@ export default function Category() {
       </div>{" "}
       <h2 className="text-3xl font-semibold m-10">{title}</h2>
       <div className=" grid grid-cols-2 sm:grid-cols-auto-fill-200 justify-evenly gap-10 flex-wrap px-10">
-        {collections.map((group: any) => {
+        {collections.map((group) => {
           return (
             <Link key={group.id} href={`/${group.url}?type=${group.title}`}>
               <Cards img="images/BLACK.jpg" key={group.id} text={group.title} />
